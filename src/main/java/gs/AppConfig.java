229413b7f075e6df44bd177b42c79329a9eee362
729123910 <729123910@qq.com>
2019-09-02 17:47:15 +0800

    123

--- src/main/java/gs/AppConfig.java
+++ src/main/java/gs/AppConfig.java
@@ -1,56 +1,56 @@
-package gs;
+// package gs;
 
-import net.sf.log4jdbc.sql.jdbcapi.DataSourceSpy;
+// import net.sf.log4jdbc.sql.jdbcapi.DataSourceSpy;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Primary;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+// import org.springframework.boot.context.properties.ConfigurationProperties;
+// import org.springframework.boot.jdbc.DataSourceBuilder;
+// import org.springframework.context.annotation.Primary;
 
-import javax.sql.DataSource;
+// import javax.sql.DataSource;
 
-import org.hibernate.SessionFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+// import org.hibernate.SessionFactory;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
 
-import javax.persistence.EntityManagerFactory;
+// import javax.persistence.EntityManagerFactory;
 
 
-@Configuration
-public class AppConfig {
+// @Configuration
+// public class AppConfig {
     
-    public AppConfig() {
-        System.out.println("TestConfiguration容器启动初始化。。。");
-    }
+//     public AppConfig() {
+//         System.out.println("TestConfiguration容器启动初始化。。。");
+//     }
 
-    @Autowired
-    DataSourceProperties dataSourceProperties;
+//     // @Autowired
+//     // DataSourceProperties dataSourceProperties;
 
-    @Bean
-    //@ConfigurationProperties(prefix = DataSourceProperties.PREFIX)
-    @ConfigurationProperties()
-    DataSource realDataSource() {
-        DataSource dataSource = DataSourceBuilder
-                .create(this.dataSourceProperties.getClassLoader())
-                .url(this.dataSourceProperties.getUrl())
-                .username(this.dataSourceProperties.getUsername())
-                .password(this.dataSourceProperties.getPassword())
-                .build();
-        return dataSource;
-    }
+//     // @Bean
+//     // //@ConfigurationProperties(prefix = DataSourceProperties.PREFIX)
+//     // @ConfigurationProperties()
+//     // DataSource realDataSource() {
+//     //     DataSource dataSource = DataSourceBuilder
+//     //             .create(this.dataSourceProperties.getClassLoader())
+//     //             .url(this.dataSourceProperties.getUrl())
+//     //             .username(this.dataSourceProperties.getUsername())
+//     //             .password(this.dataSourceProperties.getPassword())
+//     //             .build();
+//     //     return dataSource;
+//     // }
 
-    @Bean
-    @Primary
-    DataSource dataSource() {
-        return new DataSourceSpy(realDataSource());
-    }
+//     // @Bean
+//     // @Primary
+//     // DataSource dataSource() {
+//     //     return new DataSourceSpy(realDataSource());
+//     // }
 
-    @Bean
-    public SessionFactory sessionFactory(EntityManagerFactory factory) {
-        if (factory.unwrap(SessionFactory.class) == null) {
-            throw new NullPointerException("factory is not a hibernate factory");
-        }
-        return factory.unwrap(SessionFactory.class);
-    }
-}
+//     // @Bean
+//     // public SessionFactory sessionFactory(EntityManagerFactory factory) {
+//     //     if (factory.unwrap(SessionFactory.class) == null) {
+//     //         throw new NullPointerException("factory is not a hibernate factory");
+//     //     }
+//     //     return factory.unwrap(SessionFactory.class);
+//     // }
+// }
--- src/main/java/gs/utility/hibernateSessionFactory.java
+++ src/main/java/gs/utility/hibernateSessionFactory.java
@@ -1,62 +1,21 @@
 package gs.utility;
 
-import java.util.List;
-
 import javax.persistence.EntityManagerFactory;
-import javax.sql.DataSource;
-
-import com.alibaba.fastjson.JSON;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 //import gs.AppConfig;
 
-@Configuration
 public class hibernateSessionFactory {
 
     @Autowired
     private  EntityManagerFactory entityManagerFactory;
 
-    @Autowired
-    private  DataSourceProperties dataSourceProperties;
- 
-    @Autowired
-    private  ApplicationContext applicationContext;
-
-
     public  Session getSession() {
         return entityManagerFactory.unwrap(SessionFactory.class).openSession();
     }
 
-    public  void contextLoads() {
-        try {
-            //AppConfig aa=new AppConfig();
-            //DataSource dd=aa.dataSource();
-            // if (dd!=null) {
-                
-
-            // }
-            System.out.println(dataSourceProperties.getUrl());    
-        } catch (Exception e) {
-            System.out.println(e.toString());
-        }
-        
-        // // 获取配置的数据源
-        // DataSource dataSource = applicationContext.getBean(DataSource.class);
-        // // 查看配置数据源信息
-        // System.out.println(dataSource);
-        // System.out.println(dataSource.getClass().getName());
-        // System.out.println(dataSourceProperties);
-        // //执行SQL,输出查到的数据
-        // JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
-        // List<?> resultList = jdbcTemplate.queryForList("select 1 from dual");
-        // System.out.println("===>>>>>>>>>>>" + JSON.toJSONString(resultList));
-     }
 
 }
