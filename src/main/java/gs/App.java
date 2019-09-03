02173624d34be6761c20098f3a14640d53c08769
729123910 <729123910@qq.com>
2019-09-03 20:56:00 +0800

    123

--- src/main/java/gs/App.java
+++ src/main/java/gs/App.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"gs.Controller,gs.config"}) 
+@ComponentScan(basePackages = {"gs.controller,gs.config"}) 
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
